7ac449e9a21a08308f016bc8dce706eb6ff34a90
0x5abe <sabemasterprox@gmail.com>
2025-12-05 22:16:54 -0300

    3rdParty MSL: Match math_ppc.c (but can't link yet)

--- src/3rdParty/PowerPC_EABI_Support/MSL/MSL_C/PPC_EABI/Src/math_ppc.c
+++ src/3rdParty/PowerPC_EABI_Support/MSL/MSL_C/PPC_EABI/Src/math_ppc.c
@@ -1,9 +1,12 @@
 #include "math_ppc.h"
 #include "fdlibm.h"
 
-/* 8036CA30-8036CA54 367370 0024+00 0/0 1/1 0/0 .text            acosf */
-__declspec(weak) float acosf(float x) {
-    return acos(x);
+__declspec(weak) double fabs(double f) {
+    return __fabs(f);
+}
+
+__declspec(weak) float powf(float f, float f2) {
+    return pow(f, f2);
 }
 
 /* 8036CA0C-8036CA30 36734C 0024+00 0/0 4/4 0/0 .text            cosf */
@@ -14,19 +17,4 @@ __declspec(weak) float cosf(float x) {
 /* 8036C9E8-8036CA0C 367328 0024+00 0/0 4/4 0/0 .text            sinf */
 __declspec(weak) float sinf(float x) {
     return sin(x);
-}
-
-/* 8036C9C4-8036C9E8 367304 0024+00 0/0 2/2 0/0 .text            tanf */
-__declspec(weak) float tanf(float x) {
-    return tan(x);
-}
-extern float __fabsf(float);
-__declspec(weak) inline double fabs(double f) {
-    return __fabs(f);
-}
-__declspec(weak) inline double fabsf2(float f) {
-    return __fabsf(f);
-}
-__declspec(weak) inline float fabsf(float f) {
-    return fabsf2(f);
 }
